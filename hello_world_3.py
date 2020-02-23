5fa612b12c519435a7c3f9ae6ba97e1fe8e2bbd8
sihaohuanguc <shuang1024@uchicago.edu>
2020-02-23 16:49:53 -0600

    Create hello_world_3.py

---
+++ hello_world_3.py
@@ -0,0 +1,9 @@
+class say_something(object):
+    def __init__(self):
+        pass
+
+    def print_the_sentence(self,value):
+        print(value)
+        
+apple=say_something()
+apple.print_the_sentence("Hello world!")
